116343a8e5ca5fac49f418af528830d13399cf26
wuluomin <845170591@qq.com>
2019-09-05 12:53:13 +0800

    高效的验证数独方法二--hashset

---
+++ sudoku-method2.js
@@ -0,0 +1,63 @@
+function isValidSudoku(inputArr) {
+    // 定义三个二维数组，hashset
+    var rowFlag = new Boolean(9)
+    var colFlag = new Boolean(9)
+    var BlockFlag = new Boolean(9)
+    for (var i = 0; i < 9; i++) {
+        rowFlag[i] = new Boolean()
+        colFlag[i] = new Boolean()
+        BlockFlag[i] = new Boolean()
+    }
+    // 分别放数独的数字标志
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            var blockId = Math.floor(i / 3) * 3 + Math.floor(j / 3)
+            if (inputArr[i][j] >= '1' && inputArr[i][j] <= 9) {
+                var num = inputArr[i][j] - 1;
+                if (rowFlag[i][num]) {
+                    console.log(`row======${i+1}=======${num + 1}`)
+                    return false
+                }
+                if (colFlag[j][num]) {
+                    console.log(`col======${j+1}=======${num + 1}`)
+                    return false
+                }
+                if (BlockFlag[blockId][num]) {
+                    console.log(`block======${blockId+1}=====${num + 1}`)
+                    return false
+                }
+                rowFlag[i][num] = !rowFlag[i][num]
+                colFlag[j][num] = !colFlag[j][num]
+                BlockFlag[blockId][num] = !BlockFlag[blockId][num]
+            }
+        }
+    }
+    return true
+}
+
+var input1 = [
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "9", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+
+var input2 = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+
+console.log(isValidSudoku(input1));
+console.log(isValidSudoku(input2));
