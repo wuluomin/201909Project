050c0ca5682c8240bb38008c92a3ca0774562896
wuluomin <845170591@qq.com>
2019-09-09 19:14:31 +0800

    start of java program.

---
+++ src/quickstart/Helloworld.java
@@ -0,0 +1,8 @@
+package quickstart;
+
+public class Helloworld {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+            System.out.println("hhhhhhhhhh");
+        }
+}
